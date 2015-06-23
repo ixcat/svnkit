1bdc97770170992dab43d22ff2b857d8f3d7d10d
Chris Turner <cat@199tech.net>
2015-06-23 03:34:00 +0000

    svnkit/trunk: add rough-draft configure script
    this script parses arguments and drives the 'setprefix.sh' script-
    
    - testing tbd.
    - direct integration of setprefix.sh logic into configure forthcoming.

--- svnkit/setprefix.sh
+++ svnkit/setprefix.sh
@@ -9,16 +9,36 @@ files="${files} src/svn-dotprofile";
 files="${files} Makefile";
 files="${files} svnkit.txt";
 
+vars="";
+vars="${vars} SVNADMIN_HOME";
+vars="${vars} DESTPREFIX";
+vars="${vars} SVN_REALM";
+
+error_exit() {
+	echo "$*";
+	exit 1;
+}
+
+checkvar() {
+	val="`eval echo \\$$1`"; echo $val;
+	[ -z "$val" ] && error_exit "variable $1 not set ... exiting.";
+
+}
+
 substfile() {
 
 	# @SVNADMIN_HOME@ # eg. where svnadmin user homedir is
 	# @DESTPREFIX@ # e.g. usr/local /usr/pkg /usr, etc.
 	# @SVN_REALM@ # svn realm name configured - default `hostname`? 
-	sed	-e 's:@SVNADMIN_HOME@:$SVNADMIN_HOME:g' \
-		-e 's:@DESTPREFIX@:$DESTPREFIX:g' \
-		-e 's:@SVN_REALM@:$SVN_REALM:g' \
-	${1}.in
+	sed	-e "s:@SVNADMIN_HOME@:$SVNADMIN_HOME:g" \
+		-e "s:@DESTPREFIX@:$DESTPREFIX:g" \
+		-e "s:@SVN_REALM@:$SVN_REALM:g" \
+	${1}.in > ${1}
 }
+
+for v in $vars; do
+	checkvar $v;
+done
 
 for f in $files; do
 	substfile $f;
